b567ba13ad3ddc8832745066ebe9d0659c5357b0
deilor101 <matehenrique20@hotmail.com>
2023-12-13 23:03:06 -0300

    aula 03 finalizada

---
+++ CriandoArquivo.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+partial class Program
+{
+    static void CriarArquivo()
+    {
+        var caminhoNovoArquivo = "contasExportadas.csv";
+        using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+        {
+            var contaComoString = "456, 7895, 4786.40, Matheus Henrique";
+
+            var encoding = Encoding.UTF8;
+
+            var bytes = encoding.GetBytes(contaComoString);
+
+            fluxoDeArquivo.Write(bytes, 0, bytes.Length);
+        }
+    }
+
+    static void CriarArquivoComWriter()
+    {
+        var caminhoNovoArquivo = "contasExportadas.csv";
+        using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create))
+        using (var escritor = new StreamWriter(fluxoDeArquivo))
+        {
+            escritor.Write("446, 1195, 786.40, Maria Luane");
+        }
+    }
+}
